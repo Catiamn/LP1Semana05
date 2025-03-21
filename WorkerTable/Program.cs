3a08635cd4a511db483be2d249c7192199e45baa
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-21 14:53:48 +0000

    Added rows with randomized names and jobs. Added comments as well.

--- WorkerTable/Program.cs
+++ WorkerTable/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Spectre.Console;
 using Bogus;
+using Bogus.DataSets;
 
 namespace WorkerTable
 {
@@ -9,15 +10,25 @@ namespace WorkerTable
         //argumento recebido sera o numero de empregados
         private static void Main(string[] args)
         {
-        int count = int.Parse(args[0]);
+            int count = int.Parse(args[0]);
             Randomizer.Seed = new Random(count); //defir a seed
 
             Faker faker = new Faker("pt_PT"); //gerar dados atraves de faker
 
             var table = new Table();
+            //tabela com 3 colunas
             table.AddColumn("ID");
-            table.AddColumn(new TableColumn("Nome").LeftAligned());
+            table.AddColumn(new TableColumn("Name").LeftAligned());
             table.AddColumn(new TableColumn("Job").LeftAligned());
+
+            //para cada numbero criar uma linha na tabela
+            for (int i = 0; i < count; i++)
+            {
+                Name name = faker.Name;
+                table.AddRow($"{(i + 1)}", $"{name.FirstName()} {name.LastName()}", name.JobTitle());
+            }
+            //escrever a tabela
+            AnsiConsole.Write(table);
         }
     }
 }

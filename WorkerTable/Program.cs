9d5554f428ada519811ff705402505b2e1187cff
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-21 14:11:25 +0000

    Added Spectre.Console and Bogus

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Spectre.Console;
+using Bogus;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+            Randommizer.Seed = new Random(int.Parse(args[0])) //def seed
+            Faker false = new False(pt_PT) //gerar dados atraves de faker
+        }
+    }
+}

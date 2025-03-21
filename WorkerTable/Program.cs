eec9cba28227e55534226493748b072a0766d521
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-21 14:39:32 +0000

    Added the table with the titles for each section

--- WorkerTable/Program.cs
+++ WorkerTable/Program.cs
@@ -6,11 +6,18 @@ namespace WorkerTable
 {
     public class Program
     {
+        //argumento recebido sera o numero de empregados
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
-            Randommizer.Seed = new Random(int.Parse(args[0])) //def seed
-            Faker false = new False(pt_PT) //gerar dados atraves de faker
+        int count = int.Parse(args[0]);
+            Randomizer.Seed = new Random(count); //defir a seed
+
+            Faker faker = new Faker("pt_PT"); //gerar dados atraves de faker
+
+            var table = new Table();
+            table.AddColumn("ID");
+            table.AddColumn(new TableColumn("Nome").LeftAligned());
+            table.AddColumn(new TableColumn("Job").LeftAligned());
         }
     }
 }
